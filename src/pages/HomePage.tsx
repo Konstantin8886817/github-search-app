7e69e0489f471d1cfd871634dfea7cae54750ca5
Konstantin / Front-End Developer <61967294+Konstantin8886817@users.noreply.github.com>
2022-07-14 22:44:44 +0300

    added dropdown

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDebounce } from '../hooks/debounce';
 import { useSearchUserQuery } from '../store/github/github.api';
 
 export default function HomePage() {
-	const { isLoading, isError, data: userData } = useSearchUserQuery('konstantin');
+	const [search, setSearch] = useState('');
+	const [dropdown, setDropdown] = useState(false);
+	const debounce = useDebounce(search);
+	const {
+		isLoading,
+		isError,
+		data: userData,
+	} = useSearchUserQuery(debounce, {
+		skip: debounce.length < 3,
+	});
 
-	console.log(userData);
+	useEffect(() => {
+		setDropdown(debounce.length > 3 && userData?.length! > 0);
+	}, [debounce, userData]);
 
 	return (
 		<div className="flex justify-center pt-10 mx-auto h-screen w-screen">
 			{isError && <p className="text-center text-red-600">Something went wrong ...</p>}
 			<div className="relative w-[560px]">
-				<input type={'text'} className="border py-2 px-4 w-full h-[42px] mb-2" placeholder="Search for Github username..." />
-				<div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga facilis quis inventore sequi nihil! Laboriosam in corrupti itaque
-					aliquam voluptates, velit blanditiis autem minima quis quos nemo, perferendis nobis eos!
-				</div>
+				<input
+					type={'text'}
+					className="border py-2 px-4 w-full h-[42px] mb-2"
+					placeholder="Search for Github username..."
+					value={search}
+					onChange={(e) => setSearch(e.target.value)}
+				/>
+				{dropdown && (
+					<ul className="list-none absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white overflow-y-scroll">
+						{isLoading && <p className="text-center">Loading ...</p>}
+						{userData?.map((user) => (
+							<li key={user.id} className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer">
+								{user.login}
+							</li>
+						))}
+					</ul>
+				)}
 			</div>
 		</div>
 	);
